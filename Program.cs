0294347ac320ff7d9940675ceec409063d31e74d
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-28 12:20:21 +0530

    Added employee wage file

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Employee_wages
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to emplyee wage computation");
+        }
+    }
+}
