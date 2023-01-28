3657e0cacaf66584e5f6333a2ada49de707d68c3
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-28 13:16:37 +0530

    Added Uc1

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace Employee_wages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to emplyee wage computation");
+            Console.WriteLine("Welcome to Employee wage computation");
+            Console.WriteLine(" Name of employee is:");
+            string name=Console.ReadLine();
+            Random random= new Random();
+            int isEmp=random.Next(0,2);
+            if(isEmp==0)
+            {
+                Console.WriteLine(name +" Employee is present");
+            }
+            else
+            {
+                Console.WriteLine(name +" Employee is absent");
+            }
         }
     }
 }
