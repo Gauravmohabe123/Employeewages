d06950019faa3aea5cc36c8525347a81c2d175f8
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-28 19:31:34 +0530

    Added Uc2

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace Employee_wages
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to emplyee wage computation");
+            Console.WriteLine("Welcome to Employee wage computation");
+            Console.WriteLine(" Name of employee is:");
+            string name = Console.ReadLine();
+            int fullDayHour = 8;
+            int perHourWage = 20;
+            Random random = new Random();
+            int isEmp = random.Next(0, 2);
+            int DailyWage = 0;
+            if (isEmp == 0)
+            {
+
+                Console.WriteLine(name + " Employee is not Abesent ");
+                Console.WriteLine("Daily  Wage of Employee is:" + DailyWage);
+
+
+            }
+            else
+            {
+                DailyWage = fullDayHour * perHourWage;
+                Console.WriteLine(name + " Employee is Presentsent");
+                Console.WriteLine("Daily  Wage of Employee is:" + DailyWage);
+            }
         }
     }
 }
