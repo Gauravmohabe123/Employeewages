cd8530a2ae642f85e0b978e05f64327166db44a8
Gaurav Mohabe <gmohabe12@gmail.com>
2023-01-29 17:35:53 +0530

    Added Uc6

--- Program.cs
+++ Program.cs
@@ -3,10 +3,62 @@
 namespace Employee_wages
 {
     internal class Program
-    {
-        static void Main(string[] args)
+    {static void Wage_Attendance()
         {
             Console.WriteLine("Welcome to emplyee wage computation");
+
+        }
+        static void Main(string[] args)
+        {
+            Wage_Attendance();
+            int WageperHour = 20;
+            int FullDayHour = 8;
+            int partTimehour = 4;
+            int wage = 0;
+            Random rnd = new Random();
+            int monthlyWage = 0;
+            int day = 0;
+            int count = 0;
+            int absent = 0;
+            int TotalHour = 0;
+            while (day != 20 && count != 30)
+
+            {
+                int n = rnd.Next(0, 3);
+                switch (n)
+
+                {
+                    case 0:
+                        absent++;
+                        count++;
+                        break;
+
+                    case 1:
+
+                        wage = partTimehour * WageperHour;
+                        monthlyWage = wage + monthlyWage;
+                        count++;
+                        day++;
+                        TotalHour += partTimehour;
+                        break;
+                    case 2:
+                        wage = FullDayHour * WageperHour;
+                        monthlyWage = wage + monthlyWage;
+                        count++;
+                        day++
+                        TotalHour += FullDayHour;
+                        break;
+
+                }
+
+            }
+            Console.WriteLine("monthly wage :" + monthlyWage);
+            Console.WriteLine("present days :" + day);
+            Console.WriteLine("Total half day:" + partTimehour);
+            Console.WriteLine("Total full Day:" + FullDayHour);
+            Console.WriteLine("Total absent in a month:" + absent);
+            Console.WriteLine("Total Worked Hour in a month:" + TotalHour);
+            
         }
     }
 }
